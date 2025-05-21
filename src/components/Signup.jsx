b88d67f33fcec8be74bd1029b2ccd7204fcbe988
Fariha Promi <farihapromi2611@gmail.com>
2025-05-21 18:59:27 +0600

    Ad Login& signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
 
 const Signup = () => {
+  const handleSignup=e=>{
+    e.preventDefault()
+     const email=e.target.email.value;
+    const password=e.target.password.value
+  }
+
   return (
-    <div>
-        <h2>Sign up</h2>
-      
+      <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col ">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+     
     </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <form className="card-body" onSubmit={handleSignup}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" name='email'
+           className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered"
+          name="password"
+           required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button  
+   
+          className="btn btn-primary rounded-lg">Signup</button>
+        </div>
+      </form>
+      <p className="mb-4 ml-4">
+        Already  have an account?
+        <Link className='p-2 m-2 bg-blue-500 rounded-lg' to='/login'>Login</Link>
+      </p>
+    </div>
+  </div>
+</div>
   )
 }
 
