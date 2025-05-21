d7650629ca4b984414710f7431c0ab447af380f4
Fariha Promi <farihapromi2611@gmail.com>
2025-05-21 12:13:03 +0600

    Add initial setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className='bg-blue-500'>This is Home</h2>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <h2>Sign up</h2>
+      
+    </div>
+  )
+}
+
+export default Signup
