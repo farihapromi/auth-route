8f158049b21a645ddecbf04b3e88ac9b58d13ec8
Fariha Promi <farihapromi2611@gmail.com>
2025-05-21 20:17:52 +0600

    Add observer

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,9 +1,10 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
-import React, { createContext } from 'react'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
+import React, { createContext, useState } from 'react'
 import auth from '../firebase.init'
  export const AuthContext=createContext(null)
 
 const AuthProvider = ({children}) => {
+    const [user,setUser]=useState(null)
     const createUser=(email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password)
     }
@@ -12,8 +13,24 @@ const AuthProvider = ({children}) => {
     const signInUser=(email,password)=>{
         return signInWithEmailAndPassword(auth,email,password)
     }
+    //observer.store the informaiton of user
+    
+    onAuthStateChanged(auth,currentUser=>{
+        if(currentUser){
+            console.log('curent user')
+            setUser(currentUser)
+        }
+        else{
+            console.log('No user')
+            setUser(null)
+        }
+
+    })
+
+    //pass value to context
     const authInfo={
         name:"promi",
+        user,
       createUser,
        signInUser
     }
