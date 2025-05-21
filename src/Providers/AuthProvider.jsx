f4b6f72adfe13a874c1b51a72aa06c94467bbe57
Fariha Promi <farihapromi2611@gmail.com>
2025-05-21 20:11:23 +0600

    Add login with authProvider

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import React, { createContext } from 'react'
 import auth from '../firebase.init'
  export const AuthContext=createContext(null)
@@ -7,9 +7,15 @@ const AuthProvider = ({children}) => {
     const createUser=(email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password)
     }
+
+    //login
+    const signInUser=(email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
     const authInfo={
         name:"promi",
-      createUser
+      createUser,
+       signInUser
     }
   return (
     <div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Providers/AuthProvider';
 
 const Login = () => {
+   const {signInUser}=useContext(AuthContext)
+
   const handleLogin=e=>{
     e.preventDefault()
     const email=e.target.email.value;
     const password=e.target.password.value
+    signInUser(email,password)
+    .then(result=>console.log(result.user))
+    .catch(error=>console.log(error))
   }
   return (
     <div className="hero bg-base-200 min-h-screen">
